2b721b908c15e62ede8e287e4e570a47e55ca27c
EC2 Default User <ec2-user@ip-172-31-34-233.ap-northeast-1.compute.internal>
2023-01-23 02:03:49 +0000

    first submit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :is_matching_long_user, only: [:edit, :update]
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+end
+
+  def is_matching_long_user
+    user_id = params[:id].to_i
+    unless user_id == current_user
+      redirect_to post_images_path
+    end
+  end
